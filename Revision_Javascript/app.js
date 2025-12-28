31ae711a23f4c72490e2e3db04cc4dc4387bf96a
Abdullah Naseem <abdullahnaseem846@gmail.com>
2025-12-28 16:46:11 +0500

    first commit

---
+++ Revision_Javascript/app.js
@@ -0,0 +1,232 @@
+// alert("Hello! Javascript");
+
+// console.log("................................................................");
+// var x = 10;
+// console.log(x);
+
+// console.log("................................................................");
+// var firstName = "Abdullah"; //string
+// var age = 19; //number
+// var attend = true; //boleon
+// var y; //undefined
+
+// console.log(firstName);
+// console.log(age);
+// console.log(attend);
+// console.log(y);
+
+// console.log(".................................................................");
+// var second_name = "Rayyan";
+// console.log(typeof second_name, second_name);
+
+// var age2 = 18;
+// console.log(typeof age2, age2);
+
+// var attend2 = false;
+// console.log(typeof attend2, attend2);
+
+// var z;
+// console.log(typeof y, y);
+
+// console.log("...................................................................");
+// var first_name = "Ahmed";
+// var secondName = "Ali";
+
+// console.log(first_name,  secondName);
+
+// var firstName = "Ahmed";
+// var lastName = "Ali";
+
+// console.log(firstName + " " + lastName);
+
+// console.log("...................................................................");
+// var x = 10;
+// var y = "5";
+
+// console.log(x + y); // 105 (string concatenation)
+// console.log(x - y); // 5
+// console.log(x * y); // 50
+// console.log(x / y); // 2
+
+// console.log("...............................................................");
+// var add = 2 + 4; //Addition
+// var subt = 9 - 4; //Subtract
+// var mult = 4 * 9; //Multiplication
+// var divd = 10 / 2; //Divide
+// var power = 2 ** 2; //Value Power
+// var remnd = 10%3; //Reminder
+
+// console.log(add);
+// console.log(subt);
+// console.log(mult);
+// console.log(divd);
+// console.log(power);
+// console.log(remnd);
+
+// var calc1 = 2 + 2 * 2;
+// var calc2 = 2 * 2 + 2;
+// var calc3 = (2 + 2) * 2;
+
+// console.log(calc1);
+// console.log(calc2);
+// console.log(calc3);
+
+// console.log("..................................................................");
+// var value = 10;
+// var value = 20; //is tarah ke code me by default subse niche jo value hogi uska console.log hoga. upar wali value keleye upar ke value ke neche cosole.log ka tag lagana hoga phr Console me aaega.
+
+// console.log(value);
+
+// console.log("....................................................................");
+// var marks = "80";
+
+// // ==  → checks VALUE only
+// // === → checks VALUE + TYPE (Recommended)
+ 
+// console.log(marks == 80); // true
+// console.log(marks === 80); // false
+
+// var marks2 = 80;
+// // !=  → not equal
+// // !== → not equal (value + type)
+
+// console.log(marks2 !== 60); // true
+// console.log(marks2 !== 80); // false
+
+// console.log(".....................................................................");
+// var name = 'Abdullah';
+// var attend = true;
+// var marks = prompt("Please enter your number (0-100)")
+
+// if (marks >= 80 && marks <= 100) {
+//     console.log("A");
+// } else if (marks >= 70 && marks <= 79) {
+//     console.log("B");
+// } else if (marks >= 60 && marks <= 69) {
+//     console.log("C");
+// } else if (marks >= 50 && marks <= 59) {
+//     console.log("D");
+// } else if (marks >= 40 && marks <= 49) {
+//     console.log("E");
+// } else if (marks < 40 && marks >= 0) {
+//     console.log("Fail");
+// } else {
+//     alert("❌Invalid Number, Please Re-Inter your Number")
+// }
+
+// console.log("-----------------------------------------------------------------");
+// AND (&&) → All conditions must be TRUE
+// OR  (||) → At least one condition must be TRUE
+
+// var name = "Ali";
+// var attend = true;
+// var marks = "40";
+
+// // Example 01....................................
+ 
+// if (marks >= 50 || attend === true) {
+//     console.log("Student Passed");
+// } else if (marks < 50 || attend --- true) {
+//     console.log("Student Failed");
+// }
+
+// // Example 02...................................
+
+// if (marks >= 50 && attend === true) {
+//     console.log("Allowed to sit in exam");
+// } else if (marks < 50 && attend === true) {
+//     console.log("Not Allowed to sit in exam");
+// }
+
+// console.log(".....................................................................");
+// var firstName = " Muhammad ";
+
+// console.log(firstName.toLowerCase());
+// console.log(firstName.toUpperCase());
+// console.log(firstName.trimStart());
+// console.log(firstName.trimEnd());
+// console.log(firstName.toLowerCase().trim());
+
+// if (firstName.toLowerCase().trim() === "muhammad") {
+//     console.log("TRUE");
+// } else {
+//     console.log("FALSE");
+// }
+
+console.log("....................................................................");
+
+var fullName = "Ahmed Ali";
+
+// charAt (index) / [] box
+// A h m e d   A l i
+// 0 1 2 3 4 5 6 7 8
+
+// length 
+// A h m e d   A l i
+// 1 2 3 4 5 6 7 8 9
+var index = fullName.length -1;
+console.log(fullName[index]);
+
+console.log(fullName[6]);
+
+console.log(fullName.charAt(5));
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  if (age >= 18) {
+//      console.log("Eligible to vote");
+// } else {
+//      console.log("Not eligible to vote");
+// }
+// var age = true;
+
+
+// var isLoggedIn = true;
+
+// if (isLoggedIn) {
+//     console.log("User is logged in");
+// } else {
+//     console.log("User is not logged in");
+// }
+
+
+
